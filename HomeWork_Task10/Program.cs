711ce81c2d5697cf0c8220cf69b1e73c580751f2
Khursan Lyudmila <khursanlyudmila@gmail.com>
2022-12-20 10:55:39 +0300

    add Home Work Task 10

---
+++ HomeWork_Task10/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Введите число:");
+
+string numberStr = Console.ReadLine() ?? "";
+int number = int.Parse(numberStr);
+if (number >=100 && number < 1000)
+{
+    int firstDigit = number % 100;
+    int result = firstDigit / 10;
+    Console.WriteLine(result);
+}
+else
+Console.WriteLine("Число не соответствует заданным параметрам");
